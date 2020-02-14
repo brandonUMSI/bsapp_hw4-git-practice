02db10f33d7c068478028502616890b545a587d4
Brandon Sapp <brandonsapp@Brandons-MacBook-Pro-2.local>
2020-02-13 22:05:57 -0500

    Comments for name and uniqnane. New print function

--- hw4_git.py
+++ hw4_git.py
@@ -1,1 +1,8 @@
-print("Hellow World")
+# Name: Brandon Sapp
+# Uniqname: bsapp
+# Course: 507 001
+# Homework 4 Git Practice
+##################################################################
+print("Hello World")
+name = input("what is your name")
+print("Hello " + name)
